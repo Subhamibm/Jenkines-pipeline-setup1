f6571da14a75727a219100ffdf425979ca176205
Subham Roy <subham.roy@ibm.com>
2022-04-17 19:34:50 +0530

    added test method

--- src/test/java/com/ibm/JenkinsSetup1ApplicationTests.java
+++ src/test/java/com/ibm/JenkinsSetup1ApplicationTests.java
@@ -1,13 +1,34 @@
 package com.ibm;
 
+import com.ibm.controller.MyController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class JenkinsSetup1ApplicationTests {
 
+	MyController myCon = new MyController();
+
 	@Test
-	void contextLoads() {
+	void testGetData() {
+
+		String output = myCon.getData();
+		assert output!= "": "Hello, I am from getData";
+	}
+
+	@Test
+	void testGetIBM() {
+
+		String output = myCon.getData();
+		assert output!= "": "Hello, I am from IBM";
+	}
+
+	@Test
+	void testPostData() {
+
+		String output = myCon.getData();
+		assert output!= "": "Hello, I am from postData, your data is being updated...";
 	}
 
 }
